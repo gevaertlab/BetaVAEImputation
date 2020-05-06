08335717d4938af44f333ff7150a442a7bcd4c0f
linayqiu <41713996+linayqiu@users.noreply.github.com>
2020-05-06 11:04:58 -0700

    Update train_beta_VAE.py

--- train_beta_VAE.py
+++ train_beta_VAE.py
@@ -7,6 +7,7 @@ import random
 import tensorflow as tf
 import sys
 import argparse
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, default='config.json', help='configuration json file')
@@ -17,42 +18,33 @@ if __name__ == '__main__':
         with open(args.config) as f:
             config = json.load(f)
     
-        training_epochs=config["traning_epochs"] #250
+        training_epochs=config["training_epochs"] #250
         batch_size=config["batch_size"] #250
         learning_rate=config["learning_rate"] #0.0005
         latent_size = config["latent_size"] #200    
-        hidden_size_1=config["latent_size_1"]
-        hidden_size_2=config["latent_size_2"]
-        beta=config["beta"]   
-            
-        DataPath =   config["data_path"]     
-        CorruptDataPath = config["corrupt_data_path"]
-        SaveRoot = config["save_rootpath"]
+        hidden_size_1=config["hidden_size_1"]
+        hidden_size_2=config["hidden_size_2"]
+        beta=config["beta"]            
+        data_path =   config["data_path"]     
+        corrupt_data_path = config["corrupt_data_path"]
+        save_root = config["save_rootpath"]
         
-        # LOAD DATA
-        Xdata_df = pd.read_csv(DataPath)
-        Xdata = Xdata_df.values
-        del Xdata_df
-        
-        # Load data with missing values from a csv for analysis:
-        Xdata_df = pd.read_csv(CorruptDataPath)
-        Xdata_Missing = Xdata_df.values
-        del Xdata_df
-        
-        # Properties of data:
-        n_x = Xdata_Missing.shape[1] # dimensionality of data space
-        ObsRowInd = np.where(np.isfinite(np.sum(Xdata_Missing,axis=1)))
-        NanRowInd = np.where(np.isnan(np.sum(Xdata_Missing,axis=1)))
-        NanIndex = np.where(np.isnan(Xdata_Missing))
+
+        data = pd.read_csv(data_path).values
+        data_missing = pd.read_csv(corrupt_data_path).values
+
+        n_row = data_missing.shape[1] # dimensionality of data space
+        non_missing_row_ind= np.where(np.isfinite(np.sum(data_missing,axis=1)))
+        na_ind = np.where(np.isnan(data_missing))
 
         sc = StandardScaler()
-        Xdata_Missing_complete = np.copy(Xdata_Missing[ObsRowInd[0],:])
-        sc.fit(Xdata_Missing_complete)
-        Xdata_Missing[NanIndex] = 0
-        Xdata_Missing = sc.transform(Xdata_Missing)
-        Xdata_Missing[NanIndex] = np.nan
-        del Xdata_Missing_complete
-        Xdata = sc.transform(Xdata)
+        data_missing_complete = np.copy(data_missing[non_missing_row_ind[0],:])
+        sc.fit(data_missing_complete)
+        data_missing[na_ind] = 0
+        data_missing = sc.transform(data_missing)
+        data_missing[na_ind] = np.nan
+        del data_missing_complete
+        data = sc.transform(data)
 
        
         # VAE network size:
@@ -68,21 +60,21 @@ if __name__ == '__main__':
                  n_hidden_recog_2=Encoder_hidden2, # 2nd layer encoder neurons
                  n_hidden_gener_1=Decoder_hidden1, # 1st layer decoder neurons
                  n_hidden_gener_2=Decoder_hidden2, # 2nd layer decoder neurons
-                 n_input=n_x, # data input size
+                 n_input=n_row, # data input size
                  n_z=latent_size)  # dimensionality of latent space
         
         # initialise VAE:
-        vae = TFVariationalAutoencoder(network_architecture,
+        vae = VariationalAutoencoder(network_architecture,
                                      learning_rate=learning_rate, 
                                      batch_size=batch_size,istrain=True,restore_path=None,
                                      beta=beta)
         
         # train VAE on corrupted data:
-        vae = vae.train(XData=Xdata_Missing,
+        vae = vae.train(data=data_missing,
                         training_epochs=training_epochs)
                 
         saver = tf.train.Saver()
-        save_path = saver.save(vae.sess, SaveRoot+"ep"+str(training_epochs)+"_bs"+str(batch_size)+"_lr"+str(learning_rate)+"_bn"+str(latent_size)+"_cl"+corrupt_level+"_opADAM"+"_beta"+str(beta)+"_betaVAE"+".ckpt")
+        save_path = saver.save(vae.sess, save_root+"ep"+str(training_epochs)+"_bs"+str(batch_size)+"_lr"+str(learning_rate)+"_bn"+str(latent_size)+"_opADAM"+"_beta"+str(beta)+"_betaVAE"+".ckpt")
         
         
         
