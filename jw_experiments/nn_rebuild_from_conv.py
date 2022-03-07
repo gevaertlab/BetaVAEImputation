7e31ae0bc3056263a21989caad1d95c34a04b945
judewells <judewells@hotmail.com>
2022-03-07 17:16:25 +0000

    implement layers and data not yet working

--- jw_experiments/nn_rebuild_from_conv.py
+++ jw_experiments/nn_rebuild_from_conv.py
@@ -1,4 +1,8 @@
+import os
 import numpy as np
+import json
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -14,17 +18,20 @@ class Sampling(layers.Layer):
         return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
 class NetworkBuilder():
-    def __init__(self, latent_dim=2, input_shape=(28, 28, 1)):
+    def __init__(self, latent_dim=2, input_shape=(28, 28, 1), network_architecture=None):
         self.latent_dim = latent_dim
         self.input_shape = input_shape
+        self.network_architecture = network_architecture
 
     def create_encoder(self):
-
+        n_hidden_recog_1 = self.network_architecture['n_hidden_recog_1']
+        n_hidden_recog_2 = self.network_architecture['n_hidden_recog_2']
         encoder_inputs = keras.Input(shape=self.input_shape)
-        x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
-        x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
-        x = layers.Flatten()(x)
-        x = layers.Dense(16, activation="relu")(x)
+        x = layers.Dense(units=n_hidden_recog_1, activation="relu")(encoder_inputs)
+        x = layers.Dense(units=n_hidden_recog_2)(x)
+        # x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+        # x = layers.Flatten()(x)
+        # x = layers.Dense(16, activation="relu")(x)
         z_mean = layers.Dense(self.latent_dim, name="z_mean")(x)
         z_log_var = layers.Dense(self.latent_dim, name="z_log_var")(x)
         z = Sampling()([z_mean, z_log_var])
@@ -33,12 +40,16 @@ class NetworkBuilder():
         return encoder
 
     def create_decoder(self):
+        n_hidden_gener_1 = self.network_architecture['n_hidden_gener_1']
+        n_hidden_gener_2 = self.network_architecture['n_hidden_gener_1']
         latent_inputs = keras.Input(shape=(self.latent_dim,))
-        x = layers.Dense(7 * 7 * 64, activation="relu")(latent_inputs)
-        x = layers.Reshape((7, 7, 64))(x)
-        x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
-        x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
-        decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
+        x = layers.Dense(n_hidden_gener_1, activation="relu")(latent_inputs)
+        x = layers.Dense(n_hidden_gener_2, activation="relu")(x)
+        # x = layers.Reshape((7, 7, 64))(x)
+        # x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
+        # x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
+        # decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
+        decoder_outputs = layers.Dense(self.input_shape)(x) # todo in the original implementation we define a distribution on the output
         decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
         decoder.summary()
         return decoder
@@ -67,8 +78,8 @@ class VAE(keras.Model):
             z_mean, z_log_var, z = self.encoder(data)
             reconstruction = self.decoder(z)
             reconstruction_loss = tf.reduce_mean(
-                tf.reduce_sum(
-                    keras.losses.binary_crossentropy(data, reconstruction), axis=(1, 2)
+                tf.reduce_mean(
+                    keras.losses.mean_squared_error(data, reconstruction)
                 )
             )
             kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
@@ -85,13 +96,62 @@ class VAE(keras.Model):
             "kl_loss": self.kl_loss_tracker.result(),
         }
 
-(x_train, _), (x_test, _) = keras.datasets.mnist.load_data()
-mnist_digits = np.concatenate([x_train, x_test], axis=0)
-mnist_digits = np.expand_dims(mnist_digits, -1).astype("float32") / 255
+# (x_train, _), (x_test, _) = keras.datasets.mnist.load_data()
+# mnist_digits = np.concatenate([x_train, x_test], axis=0)
+# mnist_digits = np.expand_dims(mnist_digits, -1).astype("float32") / 255
+os.chdir('..')
+with open("example_config_VAE.json") as f:
+    config = json.load(f)
 
-network_builder  = NetworkBuilder(latent_dim=2)
+training_epochs = config["training_epochs"]  # 250
+batch_size = config["batch_size"]  # 250
+learning_rate = config["learning_rate"]  # 0.0005
+latent_size = config["latent_size"]  # 200
+hidden_size_1 = config["hidden_size_1"]
+hidden_size_2 = config["hidden_size_2"]
+beta = config["beta"]
+data_path = config["data_path"]
+corrupt_data_path = config["corrupt_data_path"]
+save_root = config["save_rootpath"]
+
+
+data = pd.read_csv(data_path).values
+data_missing = pd.read_csv(corrupt_data_path).values
+# How many genes do we have? ie. what is the dimensiontality of Yobs?
+n_row = data_missing.shape[1]  # dimensionality of data space
+
+network_architecture = \
+    dict(n_hidden_recog_1=hidden_size_1,  # 1st layer encoder neurons
+         n_hidden_recog_2=hidden_size_2,  # 2nd layer encoder neurons
+         n_hidden_gener_1=hidden_size_2,  # 1st layer decoder neurons
+         n_hidden_gener_2=hidden_size_1,  # 2nd layer decoder neurons
+         n_input=n_row,  # data input size
+         n_z=latent_size)  # dimensionality of latent space
+
+# Store the index of each sample that is complete
+non_missing_row_ind = np.where(np.isfinite(np.sum(data_missing, axis=1)))
+# Store the rows and columns of every missing data point in your "data_missing" numpy array
+na_ind = np.where(np.isnan(data_missing))
+
+sc = StandardScaler()
+# Create a new numpy array that is complete (subset of simulated data_missing)
+data_missing_complete = np.copy(data_missing[non_missing_row_ind[0], :])
+# Find scaling factors from the complete set of the simulated missing data
+sc.fit(data_missing_complete)
+data_missing[na_ind] = 0  # Assign zero values to missing value indicies
+# Transform missing data by the scaling factors defined from all complete values
+data_missing = sc.transform(data_missing)
+# Re-assign the missing values to the same positions as before
+data_missing[na_ind] = np.nan
+del data_missing_complete
+
+# Remove strings and metadata from first few columns in data
+data = np.delete(data, np.s_[0:4], axis=1)
+data = sc.transform(data)
+
+network_builder  = NetworkBuilder(latent_dim=2, input_shape=n_row, network_architecture=network_architecture)
 encoder = network_builder.create_encoder()
 decoder = network_builder.create_decoder()
 vae = VAE(encoder, decoder)
-vae.compile(optimizer=keras.optimizers.Adam())
-vae.fit(mnist_digits, epochs=30, batch_size=128)
+vae.compile(optimizer=keras.optimizers.Adam(learning_rate=0.00001))
+vae.fit(data_missing, epochs=30, batch_size=128)
