1cb3df56a1ba86996d7e92ec3209f2ba189d98d5
Breeshey Roskams-Hieter <breesheyroskams-hieter@Breesheys-MacBook-Pro.local>
2022-03-10 16:28:35 +0000

    Updated X_hat_distribution to scale by standard deviation rather than variance

--- autoencodersbetaVAE.py
+++ autoencodersbetaVAE.py
@@ -112,7 +112,7 @@ class VariationalAutoencoder(object):
         # 1. Reconstruction loss - the negative log probability of of the input under the reconstructed Bernoulli distribution
         # induced by the decoder in the latent space 
         X_hat_distribution = Normal(loc=self.x_hat_mean,
-                                    scale=tf.exp(self.x_hat_log_sigma_sq))
+                                    scale=tf.sqrt(tf.exp(self.x_hat_log_sigma_sq))) # not taking square root here
         reconstr_loss = \
             -tf.reduce_sum(X_hat_distribution.log_prob(self.x), 1)
           
@@ -186,6 +186,10 @@ class VariationalAutoencoder(object):
             # Take average of absolute values across all values different between reconstructed data from previous step
             vals = np.abs(data_reconstruct[na_ind] - data_miss_val[na_ind])
             convergence.append(np.mean(vals))
+
+            # l2 
+            #vals_l2 = (data_reconstruct[na_ind] - data_miss_val[na_ind])**2
+            #convergence_l2.append(np.sqrt(np.mean(vals_l2)))
             # Replace values in data_miss_val with reconstructed values at NA indices
             data_miss_val[na_ind] = data_reconstruct[na_ind]
         
