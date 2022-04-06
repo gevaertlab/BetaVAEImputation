8fe0ad60ea0493bd6019c749222a927e1714d8a5
judewells <judewells@hotmail.com>
2022-04-06 09:37:43 +0100

    merging changes from jw just on the helper_functions file

--- lib/helper_functions.py
+++ lib/helper_functions.py
@@ -3,11 +3,40 @@ import json
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
-from autoencodersbetaVAE import VariationalAutoencoder
+import pandas as pd
+import json
+try:
+    with open("example_config_VAE.json") as f:
+        config = json.load(f)
+except:
+    with open("../example_config_VAE.json") as f:
+        config = json.load(f)
+data_path = config["data_path"]
+corrupt_data_path = config["corrupt_data_path"]
+from sklearn.preprocessing import StandardScaler
+
+def get_scaled_data():
+    for _ in range(3):
+        if os.getcwd().split('/')[-1] == 'BetaVAEImputation':
+            break
+        os.chdir('..')
+    data = pd.read_csv(data_path).values
+    data_missing = pd.read_csv(corrupt_data_path).values
+    non_missing_row_ind = np.where(np.isfinite(data_missing).all(axis=1))
+    na_ind = np.where(np.isnan(data_missing))
+    sc = StandardScaler()
+    data_missing_complete = np.copy(data_missing[non_missing_row_ind[0], :])
+    sc.fit(data_missing_complete)
+    data_missing[na_ind] = 0
+    data_missing = sc.transform(data_missing)
+    del data_missing_complete
+    data = np.array(np.copy(data[:,4:]),dtype='float64')
+    data = sc.transform(data)
+    return data, data_missing
 
 
 def apply_scaler(data, data_missing, return_scaler=False):
-    non_missing_row_ind = np.where(np.isfinite(np.sum(data_missing, axis=1)))
+    non_missing_row_ind = np.where(np.isfinite(data_missing).all(axis=1))
     na_ind = np.where(np.isnan(data_missing))
     sc = StandardScaler()
     data_missing_complete = np.copy(data_missing[non_missing_row_ind[0], :])
@@ -24,6 +53,7 @@ def apply_scaler(data, data_missing, return_scaler=False):
         return data, data_missing
 
 def load_saved_model(config_path = 'JW_config_VAE.json'):
+    from autoencodersbetaVAE import VariationalAutoencoder
     n_col = 17175
     running_directory = os.getcwd()
     if running_directory.split('/')[-1] != 'BetaVAEImputation':
@@ -59,3 +89,15 @@ def load_saved_model(config_path = 'JW_config_VAE.json'):
                                  batch_size=batch_size, istrain=False, restore_path=rp, beta=beta)
     os.chdir(running_directory)
     return vae
+
+
+def add_zero_mask(x, col_prop=0.1, row_prop=0.2):
+    """
+    Changes a randomly selected proportion of the data to zero to simulate missing values
+    """
+    n_miss_cols = int(x.shape[1]*col_prop)
+    n_miss_rows = int(x.shape[0]*row_prop)
+    miss_cols = np.array([np.random.choice(x.shape[1], size=n_miss_cols, replace=False) for _ in range(n_miss_rows)]).reshape(-1)
+    miss_rows = np.repeat(np.random.choice(x.shape[0], size=n_miss_rows, replace=False), repeats=n_miss_cols)
+    x[(miss_cols, miss_rows)] = 0
+    return x
