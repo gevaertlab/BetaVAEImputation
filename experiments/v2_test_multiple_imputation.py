8ebf3c92d991307bc7416b7a66ec1939a4f5d9e7
Jude <judewells@hotmail.com>
2022-04-12 20:56:08 +0100

    rename folder from jw_experiments to experiments

--- experiments/v2_test_multiple_imputation.py
+++ experiments/v2_test_multiple_imputation.py
@@ -37,20 +37,22 @@ def evaluate_coverage(multi_imputes=None, data=None, data_missing=None, scaler=N
 
 
 if __name__=="__main__":
-    model_dir = '../output/non_masked_beta2_lr1e-05/epoch_340_loss_7053/'
+    model_dir = '/home/jwells/Documents/BetaVAEImputation/output/non_masked_beta50_lr1e-05/epoch_1000_loss_14374.0/'
     output_dir = '/'.join(model_dir.split('/')[:-2]) + '/'
     encoder_path = model_dir + 'encoder.keras'
     decoder_path = model_dir +'decoder.keras'
     model = load_model_v2(encoder_path=encoder_path, decoder_path=decoder_path)
     data, data_missing, scaler = get_scaled_data(put_nans_back=True, return_scaler=True)
-    m_datasets = 40
+    m_datasets = 80
     np.isnan(data_missing).any(axis=0)
     missing_rows = np.where(np.isnan(data_missing).any(axis=1))[0]
     na_ind = np.where(np.isnan(data_missing[missing_rows]))
     multi_imputes = []
     for i in range(m_datasets):
-        index_changes, missing_imputed = model.impute_multiple(data_corrupt=data_missing, max_iter=200)
+        index_changes, missing_imputed = model.impute_multiple(data_corrupt=data_missing, max_iter=400)
         multi_imputes.append(missing_imputed[na_ind])
+    plt.hist(index_changes, bins=133, range=[0,132])
+    plt.show()
     evaluate_coverage(multi_imputes, data, data_missing, scaler)
     os.makedirs(output_dir, exist_ok=True)
     with open(output_dir + 'multi_impute.pickle', 'wb') as filehandle:
