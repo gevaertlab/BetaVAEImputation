01763f849eb282b10eab183985182d80a2a6b602
s2223108 <b.j.roskams-hieter@sms.ed.ac.uk>
2022-04-26 18:37:48 +0100

    Fix bug associated with improper na indexing

--- evaluate_metropolis_gibbs_eddie.py
+++ evaluate_metropolis_gibbs_eddie.py
@@ -22,24 +22,24 @@ if __name__=="__main__":
     decoder_path = model_dir +'20220423-14:22:36_decoder.keras'
     model = load_model_v2(encoder_path=encoder_path, decoder_path=decoder_path)
     data, data_missing, scaler = get_scaled_data(put_nans_back=True, return_scaler=True)
-    np.isnan(data_missing).any(axis=0) # todo remove this line of code?
     missing_rows = np.where(np.isnan(data_missing).any(axis=1))[0]
     na_ind = np.where(np.isnan(data_missing[missing_rows]))
    
     # impute by metropolis-within-Gibbs 
-    missing_imputed, convergence_loglik = model.impute_multiple(data_corrupt=data_missing, max_iter=10000,
+    missing_imputed, convergence_loglik = model.impute_multiple(data_corrupt=data_missing, max_iter=5,
                                                                method="Metropolis-within-Gibbs")
 
     # export output of m-th dataset
     data = scaler.inverse_transform(data)
     missing_imputed = scaler.inverse_transform(missing_imputed)
 
-    na_indices = pd.DataFrame({'true_values': data[na_ind], outname: missing_imputed[na_ind]})
+    truevals_data_missing = data[missing_rows]
+    na_indices = pd.DataFrame({'true_values': truevals_data_missing[na_ind], outname: missing_imputed[na_ind]})
 
     os.makedirs(output_dir, exist_ok=True)
 
     na_indices.to_csv(output_dir+'NA_imputed_values_' + outname + '.csv')
     np.savetxt(output_dir + outname + ".csv", missing_imputed, delimiter=",")
 
-    print("Mean Absolute Error:", sum(((missing_imputed[na_ind] - data[na_ind])**2)**0.5)/len(na_ind[0]))
+    print("Mean Absolute Error:", sum(((missing_imputed[na_ind] - truevals_data_missing[na_ind])**2)**0.5)/len(na_ind[0]))
 
