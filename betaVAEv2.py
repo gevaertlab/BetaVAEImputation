cba98c0372169389f1e5ad14bfad08e7b60b2993
judewells <judewells@hotmail.com>
2022-04-27 13:18:33 +0100

    adding compatibility on impute single

--- betaVAEv2.py
+++ betaVAEv2.py
@@ -211,7 +211,7 @@ class VariationalAutoencoderV2(tf.keras.Model):
             x_hat_mu, x_hat_log_var = self.decoder(z_mean)
         return x_hat_mu, x_hat_log_var # todo when implementing multiple imputation, will have to sample from N(x_hat_mu, x_hat_log_var)
 
-    def impute_single(self, data_corrupt, data_complete, n_recycles=3, loss='RMSE', scaler=None):
+    def impute_single(self, data_corrupt, data_complete, n_recycles=3, loss='RMSE', scaler=None, return_losses=False):
         assert data_complete.shape == data_corrupt.shape
         losses = []
         missing_row_ind = np.where(np.isnan(data_corrupt).any(axis=1))[0]
@@ -239,7 +239,10 @@ class VariationalAutoencoderV2(tf.keras.Model):
             elif loss =='all':
                 multi_loss_dict = calculate_losses(target_values, predictions)
                 losses.append(multi_loss_dict)
-        return data_miss_val
+        if return_losses:
+            return data_miss_val, losses
+        else:
+            return data_miss_val
 
     def impute_multiple(self, data_corrupt, max_iter=10, m = 50, method = 'pseudo-Gibbs'):
         missing_row_ind = np.where(np.isnan(data_corrupt).any(axis=1))
