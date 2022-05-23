82869f92e4005a60a02139dad444502a6494362b
judewells <judewells@hotmail.com>
2022-05-23 19:26:17 +0100

    notebook for plotting and high beta analysis script

--- experiments/array_beta_analysis_high_betas.py
+++ experiments/array_beta_analysis_high_betas.py
@@ -34,20 +34,15 @@ if __name__=="__main__":
     args = sys.argv
     d_index = int(args[1]) -1
     data, data_missing_nan, scaler = get_scaled_data(put_nans_back=True, return_scaler=True)
-    data_complete = np.copy(data)
-    missing_row_ind = np.where(np.isnan(data_missing_nan).any(axis=1))[0]
-    data_w_missingness = data_missing_nan[missing_row_ind]
-    na_ind = np.where(np.isnan(data_w_missingness))
-    data_missing = np.nan_to_num(data_missing_nan)
+    validation_w_nan, validation_complete, val_na_ind = get_additional_masked_data(data_missing_nan)
+    other_missing_row_ind = np.where(np.isnan(data_missing_nan).any(axis=1))[0]
+    training_input = np.append(validation_w_nan, data_missing_nan[other_missing_row_ind], axis=0)
+    training_input = np.nan_to_num(training_input)
     n_col = data.shape[1]
-    beta_rates = [0.1, 0.5, 1, 1.25, 1.5, 1.75, 2, 2.5, 3, 4, 5, 6, 8, 12, 16, 24, 32, 50, 64, 100, 150]
-    epoch_granularity = {0.1: 15, 0.5: 20, 1: 20, 1.25: 20, 1.5: 25, 1.75: 25, 2: 25, 2.5: 30, 3: 30, 4: 30, 5: 30,
-                         6: 30, 8: 30, 12: 30, 16: 30, 24: 30, 32: 35, 50: 40, 64: 50, 100: 100, 150: 100}
-
-    n_epochs_dict = {0.1: 300, 0.5: 300, 1: 300, 1.25: 300, 1.5: 350, 1.75: 350, 2: 400, 2.5: 400, 3: 400, 4: 400,
-                     5: 450, 6: 500, 8: 500, 12: 600, 16: 650, 24: 700, 32: 900, 50: 1100, 64: 1200, 100: 1400,
-                     150: 1600}
+    beta_rates = [4, 6, 8, 12, 16, 24, 32, 64, 100]
     beta = beta_rates[d_index]
+    dropout = False
+
     model_settings = \
         dict(n_hidden_recog_1=6000,  # 1st layer encoder neurons
              n_hidden_recog_2=2000,  # 2nd layer encoder neurons
@@ -57,24 +52,26 @@ if __name__=="__main__":
              n_z=200, # dimensionality of latent space
              )
     model_settings['beta'] = beta
-    lr = 0.00001
+
+    lr = 0.00005
     model = VariationalAutoencoderV2(model_settings=model_settings)
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=lr, clipnorm=1.0))
     # model_savepath = f'output/dropout_rate{dropout_rate}_beta{beta}_lr{lr}/'
     # os.makedirs(model_savepath, exist_ok=True)
+    epoch_granularity = {4:60, 6:60, 8:60, 12:60, 16:60, 24:60, 32:70, 50:120, 64:150, 100:250}
+    n_epochs_dict = {4:1200,  6:2000, 8:2000, 12:2500, 16:2600, 24:2800, 32:3600, 50:4000, 64:4500, 100:4500}
     epochs = epoch_granularity[beta]
     rounds = int(n_epochs_dict[beta] / epochs) + 1
     for i in range(rounds):
-        full_w_zeros = np.copy(data_missing) # 667 obs
-        full_complete = np.copy(data_complete) #667 obs
-        missing_w_nans = np.copy(data_w_missingness)
-        missing_complete = np.copy(data_complete[missing_row_ind])
-        history = model.fit(x=full_w_zeros, y=full_w_zeros, epochs=epochs, batch_size=256)
+        training_w_zeros = np.copy(training_input) # 667 obs
+        validation_w_nan_cp = np.copy(validation_w_nan)
+        history = model.fit(x=training_w_zeros, y=training_w_zeros, epochs=epochs, batch_size=256)
         loss = int(round(history.history['loss'][-1] , 0))#  callbacks=[tensorboard_callback]
         if loss < 1000:
             break
-        results = evaluate_model(model, missing_w_nans, missing_complete, na_ind, scaler)
+        results = evaluate_model(model, validation_w_nan_cp, validation_complete, val_na_ind, scaler)
         completed_epochs = (i + 1) * epochs
-        save_results(results, completed_epochs, beta, results_path='beta_analysis3.csv')
+        save_results(results, completed_epochs, beta, results_path='beta_analysis4.csv')
         remove_lock()
+
 
